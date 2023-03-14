e41b51997e74cf7ef1f683c5df3b3f3411652e0e
louay <louay.abidi@esprit.tn>
2023-03-14 22:02:46 +0100

    seconde push

--- connection.cpp
+++ connection.cpp
@@ -1,4 +1,4 @@
-#include "connection.h"
+/*#include "connection.h"
 
 Connection::Connection()
 {
@@ -27,3 +27,38 @@ void Connection::closecnnection()
 {
     db.close();
 }
+*/
+#include "connection.h"
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnection()
+{
+    db = QSqlDatabase::addDatabase("QODBC");
+
+    bool test=false;
+    db.setDatabaseName("source");
+    db.setUserName("lolita");//inserer nom de l'utilisateur
+    db.setPassword("esprit18");//inserer mot de passe de cet utilisateur
+
+    if (db.open())
+    {
+        qDebug() << "Connexion reussie !";
+        test = true;
+    }
+    else
+    {
+        qDebug() << "Erreur de connexion : " << db.lastError().text();
+    }
+
+    return test;
+}
+
+void Connection::closecnnection()
+{
+    db.close();
+}
+
